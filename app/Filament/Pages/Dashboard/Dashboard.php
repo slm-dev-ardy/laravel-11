b8d0c6f55c054afcb86a6fb3576434c8040ae5ec
priyo-ardy <priyo.ardy@hotmail.com>
2025-12-05 16:32:35 +0700

    Adding filament

---
+++ app/Filament/Pages/Dashboard/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Pages\Dashboard;
+
+use Filament\Pages\Page;
+use Livewire\Livewire;
+
+class Dashboard extends Page
+{
+    protected string $view = 'filament.pages.dashboard.dashboard';
+}
